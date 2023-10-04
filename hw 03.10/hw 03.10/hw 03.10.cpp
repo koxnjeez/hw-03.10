ee4703d72a96613a7cf79db5f91aa59b2ac6f677
koxnjeez <145009796+koxnjeez@users.noreply.github.com>
2023-10-05 01:58:59 +0300

    Add files via upload

---
+++ hw 03.10/hw 03.10/hw 03.10.cpp
@@ -0,0 +1,34 @@
+﻿#include "settings.h"
+
+void reverse(double arr[], int count) {
+    double arr2[5] = {};
+    for (int i = 0; i < 5; i++) {
+        arr2[i] = arr[count];
+        count--;
+        cout << arr2[i] << " ";
+    }
+}
+
+void reverse(double arr2[]) {
+    int count = 5;
+    double arr[5] = {};
+    for (int i = 0; i < 5; i++) {
+        arr2[i] = arr[count];
+        count--;
+        cout << arr2[i] << " ";
+    }
+}
+
+void reverse(double arr[], int i, int count) {
+    double arr2[5] = {};
+    for (i = 0; i < 5; i++) {
+        arr2[i] = arr[count];
+        count--;
+        cout << arr2[i] << " ";
+    }
+}
+
+int main()
+{
+    reverse();
+}
